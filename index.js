c40c3c40775d61ec91c118a928cfaee7e5902e28
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-02-02 23:08:50 +0000

    Add a fetch API to test if its working

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+fetch('https://api.github.com/users/AnthonyKamanya/repos',GET)
+.then((response)=>{
+    if(!response.ok){
+        alert("Sorry please try again")
+    }
+    return response.json
+})
+.catch(()=>{})
+.finally(()=>{})
